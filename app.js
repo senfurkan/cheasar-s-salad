dbf16b25368647c2576a599a72bf5c1f4080d16b
furka <furkansen202@gmail.com>
2022-10-16 11:13:10 +0300

    js

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function btnPlus(){
+    var plus = document.getElementById("input__box")
+    plus.value=Number(plus.value)+1
+   
+}
+
+function btnAdd(){
+    var add = document.getElementById('input__box')
+    window.alert("Entered Value:" + add.value)
+}
